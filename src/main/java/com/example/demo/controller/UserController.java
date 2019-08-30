0e9007bef94b99880d38f19d2e1d359dfc47c848
ShaoQinnan <qinnan.shao@siebre.com>
2019-08-30 17:13:28 +0800

    UserController update method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.User;
 import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -21,9 +28,24 @@ public class UserController {
 
     }
 
+    @GetMapping("list")
+    @ResponseBody
+    public List<User> getAllUser(HttpServletRequest request){
+        HttpSession session=request.getSession();
+        if(session.getAttribute("name")!=null&& session.getAttribute("name").equals("fkk")){
+            return userMapper.getUserInfo();
+        }else {
+            throw new RuntimeException();
+        }
+
+
+    }
+
     @GetMapping("addUser")
     @ResponseBody
-    public int addUser(String name,int id){
+    public int addUser(String name,int id,HttpServletRequest request){
+        HttpSession session=request.getSession();
+        session.setAttribute("name","fkk");
         return userMapper.addUser(id,name);
 
     }
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.example.demo.mapper;
 
+import com.example.demo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -16,4 +18,7 @@ public interface UserMapper {
 
     @Insert("insert into user values(#{id},#{name})")
     public int addUser(@Param("id") int id, @Param("name") String name);
+
+    @Select("select * from user")
+    public List<User> getUserInfo();
 }
